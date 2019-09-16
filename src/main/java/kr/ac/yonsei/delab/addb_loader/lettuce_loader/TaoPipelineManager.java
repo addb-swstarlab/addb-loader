01cc336ad001b7e511f4efb3409c1ffde7cfd9e4
totorody <arbc139@gmail.com>
2019-09-16 17:47:38 +0900

    [TRIVIAL] Code refactoring on LettuceLoader.

--- src/main/java/kr/ac/yonsei/delab/addb_loader/lettuce_loader/TaoPipelineManager.java
+++ src/main/java/kr/ac/yonsei/delab/addb_loader/lettuce_loader/TaoPipelineManager.java
@@ -11,16 +11,20 @@ import lombok.Getter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
 
 public class TaoPipelineManager {
     final private static int PIPELINE_COUNT = 1000;
+
+    ExecutorService service = null;
 
     @Getter
     public StatefulRedisConnection<String, String> nodeConn;
     @Getter
     public RedisAsyncCommands<String, String> commands;
-    private List<RedisFuture<String>> futures;
+    private List<FpWriteArgs> argsList;
 
     public static TaoPipelineManager create(StatefulRedisClusterConnection<String, String> conn,
                                             int slot) {
@@ -28,33 +32,58 @@ public class TaoPipelineManager {
         return new TaoPipelineManager(conn.getConnection(node.getNodeId()));
     }
 
+    public static TaoPipelineManager create(StatefulRedisClusterConnection<String, String> conn,
+                                            int slot,
+                                            ExecutorService service) {
+        RedisClusterNode node = conn.getPartitions().getPartitionBySlot(slot);
+        return new TaoPipelineManager(conn.getConnection(node.getNodeId()), service);
+    }
+
     public TaoPipelineManager(StatefulRedisConnection<String, String> nodeConn) {
         this.nodeConn = nodeConn;
         this.commands = nodeConn.async();
         this.commands.setAutoFlushCommands(false);
-        this.futures = new ArrayList<>();
+        this.argsList = new ArrayList<>();
+    }
+
+    public TaoPipelineManager(StatefulRedisConnection<String, String> nodeConn,
+                              ExecutorService service) {
+        this.nodeConn = nodeConn;
+        this.commands = nodeConn.async();
+        this.commands.setAutoFlushCommands(false);
+        this.argsList = new ArrayList<>();
+        this.service = service;
     }
 
     public void fpwrite(FpWriteArgs args) {
-        if (futures.size() > PIPELINE_COUNT) {
-            commands.flushCommands();
-            sync();
-            futures.clear();
+        if (argsList.size() >= PIPELINE_COUNT) {
+            if (service != null) {
+                service.execute(() -> pipeline(new ArrayList<>(argsList)));
+            } else {
+                pipeline(argsList);
+            }
+            argsList.clear();
+            argsList = new ArrayList<>();
         }
 
-        futures.add(commands.fpwrite(args));
+        argsList.add(args);
     }
 
-    public void sync() {
+    private void pipeline(List<FpWriteArgs> argsList) {
+        List<RedisFuture<String>> futures = argsList.stream()
+                .map(_args -> commands.fpwrite(_args))
+                .collect(Collectors.toList());
+        commands.flushCommands();
         LettuceFutures.awaitAll(
                 1,
                 TimeUnit.HOURS,
                 futures.toArray(new RedisFuture[0])
         );
+        futures.clear();
+        argsList.clear();
     }
 
     public void close() {
-        sync();
-        nodeConn.close();
+        pipeline(argsList);
     }
 }
